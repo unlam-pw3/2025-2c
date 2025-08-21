d3c1cf96fac9bc8a45082be904125a897fd79929
EricNahuel2002 <ericaquino2002@gmail.com>
2025-08-21 14:38:16 -0300

    se agrega la logica del juego adivinar numero con tests

---
+++ Clase1/Clase1.Logica/JuegoAdivinarNumero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Logica;
+
+public class JuegoAdivinarNumero {
+
+    private readonly int numeroEsperado;
+    public JuegoAdivinarNumero(int numeroEsperado) => this.numeroEsperado = numeroEsperado;
+
+    public string RealizarIntento(int numeroIndicado)
+    {
+        int diferencia = numeroEsperado - numeroIndicado;
+        if (diferencia == 0)
+            return "ACIERTO";
+        if (diferencia <= 5)
+            return "MUY CALIENTE";
+        if (diferencia <= 15)
+            return "CALIENTE";
+        return diferencia <= 30 ? "TIBIO" : "FRIO";
+    }
+}
---
+++ Clase1/Clase1.Tests/JuegoAdivinarNumeroTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clase1.Logica;
+
+namespace Clase1.Tests;
+
+public class JuegoAdivinarNumeroTest
+{
+    [Theory]
+    [InlineData(5,5, "ACIERTO")]
+    [InlineData(50, 5, "FRIO")]
+    [InlineData(50, 34, "TIBIO")]
+    [InlineData(50, 38, "CALIENTE")]
+    [InlineData(50, 48, "MUY CALIENTE")]
+    public void SiUnJugadorRealizaIntentoQueSeLeMuestreElMensajeAdecuado(int numeroEsperado, int numeroIndicado, string mensajeEsperado)
+    {
+        JuegoAdivinarNumero juego = new JuegoAdivinarNumero(numeroEsperado);
+
+        string acierto = juego.RealizarIntento(numeroIndicado);
+
+        Assert.Equal(mensajeEsperado,acierto);
+    }
+
+}
