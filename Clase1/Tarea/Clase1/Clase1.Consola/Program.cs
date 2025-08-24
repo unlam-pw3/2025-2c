7c5ea580563d2f84aeabd7a74a1326667799dedf
MRRegner <rodriguez.regner@gmail.com>
2025-08-24 20:26:54 -0300

    Tarea Clase1: Juego Adivinar Numero

---
+++ Clase1/Tarea/Clase1/Clase1.Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using Clase1.Logica;
+
+JuegoAdivinarNumero juegoAdivinarNumero = new JuegoAdivinarNumero();
+
+Console.WriteLine("Bienvenido a adivinar numero");
+
+bool juegoEnCurso = true;
+
+while (juegoEnCurso)
+{
+    Console.Write("Introduce un numero entre 1 y 100: ");
+    string entrada = Console.ReadLine();
+    if (int.TryParse(entrada, out int numero))
+    {
+        string resultado = juegoAdivinarNumero.Adivinar(numero);
+        Console.WriteLine(resultado);
+        if (resultado.StartsWith("Adivinaste"))
+        {
+            juegoEnCurso = false;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Por favor, introduce un numero valido.");
+    }
+}
