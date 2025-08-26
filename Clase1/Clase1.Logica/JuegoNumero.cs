a531969373683b218094b2e6e02fd5d0690deb4f
giuliannadr <giulianadirocco1@gmail.com>
2025-08-26 11:33:23 -0300

    tarea adivinar numero terminada

---
+++ Clase1/Clase1.Logica/JuegoNumero.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace Clase1.Logica;
+
+public class JuegoNumero
+{
+    private int _numeroSecreto;
+    public bool JuegoTerminado { get; private set; } = false; // estado de la partida
+
+    public void ElegirNumeroAleatorio(int min = 1, int max = 100)
+    {
+        Random rand = new Random();
+        _numeroSecreto = rand.Next(min, max + 1);
+        JuegoTerminado = false; // reinicia al empezar
+    }
+
+    public string AdivinarNumero(int numero)
+    {
+        int diferencia = Math.Abs(_numeroSecreto - numero);
+
+        if (diferencia == 0)
+        {
+            JuegoTerminado = true; // marcamos que terminó
+            return "Ganaste!";
+        }
+        if (diferencia <= 5)
+            return "Muy Caliente";
+        if (diferencia <= 15)
+            return "Caliente";
+        if (diferencia <= 30)
+            return "Tibio";
+
+        return "Frío";
+    }
+}
