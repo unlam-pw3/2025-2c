f61e77cb844a167751dae821ebdbb4b68591fb37
Nicolas Paone <nicolasarielpaone@gmail.com>
2025-08-26 10:42:40 -0300

    Desarrollo de la tarea de la clase 1

--- Clase1/Clase1.Consola/Program.cs
+++ Clase1/Clase1.Consola/Program.cs
@@ -1,40 +1,16 @@
-﻿using Clase1.Logica;
+﻿using TareaClase1.Logica;
 
-//Console.WriteLine("Hello, World!");
+AdivinarNumero juegoAdivinarNumero = new AdivinarNumero();
+int numeroRandom = juegoAdivinarNumero.ObtenerNumeroRandom();
+int numeroInicial = 0;
 
-//logica para juego ahorcado
-//deberia pedir una palabra y ocultarla con guiones bajos
-//en caso de que el usuario acierte una letra,
-//se debe mostrar la letra en todas las posiciones correspondiente
-//si el usuario falla, se debe mostrarse que ha fallado
-//con un contador de fallos y un dibujo del ahorcado
-//son 6 fallos permitidos
-
-JuegoAhorcado juegoAhorcado = new JuegoAhorcado();
-juegoAhorcado.ElegirPalabra();
-Console.WriteLine("Palabra oculta: " + juegoAhorcado.ObtenerPalabraOculta());
-Console.WriteLine("Tienes " + juegoAhorcado.FallosPermitidos() + " intentos para adivinar la palabra.");
-int fallosPermitidos = juegoAhorcado.FallosPermitidos();
-
-while (juegoAhorcado.ObtenerFallos() < fallosPermitidos && !juegoAhorcado.JuegoTerminado())
+while(!juegoAdivinarNumero.FinalizoElJuego(numeroRandom, numeroInicial))
 {
-    Console.Write("Introduce una letra: ");
-    char letra = Console.ReadKey().KeyChar;
-    Console.WriteLine();
+    Console.WriteLine("Introduci un numero: ");
+    int numeroIngresado = int.Parse(Console.ReadLine());
+    numeroInicial = numeroIngresado;
 
-    if(juegoAhorcado.AdivinarLetra(letra))
-    {
-        if(juegoAhorcado.JuegoTerminado())
-        {
-            Console.WriteLine("¡Felicidades! Has adivinado la palabra: " + juegoAhorcado.ObtenerPalabraOculta());
-            return; // Termina el juego si se adivina la palabra
-        }
-
-        Console.WriteLine("¡Correcto! La palabra oculta ahora es: " + juegoAhorcado.ObtenerPalabraOculta());
-    }
-    else
-    {
-        Console.WriteLine($"Fallaste! Te quedan {fallosPermitidos - juegoAhorcado.ObtenerFallos()} intentos.");
-        Console.WriteLine("La palabra oculta es: " + juegoAhorcado.ObtenerPalabraOculta());
-    }
+    Console.WriteLine(juegoAdivinarNumero.DeterminarDiferencia(numeroRandom, numeroIngresado));
 }
+
+Console.WriteLine("El juego terminó, el número era " + numeroRandom);
---
+++ Clase1/Clase1.Logica/AdivinarNumero.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+
+namespace TareaClase1.Logica;
+
+public class AdivinarNumero
+{
+    private int _numeroMinimo = 1;
+    private int _numeroMaximo = 100;
+
+    public int ObtenerNumeroRandom()
+    {
+        Random random = new Random();
+        return random.Next(_numeroMinimo, _numeroMaximo);
+    }
+
+    public string DeterminarDiferencia(int _numeroRandom, int _numeroIngresado)
+    {
+        int diferencia = Math.Abs(_numeroRandom - _numeroIngresado);
+        if(diferencia > 30)
+        {
+            return "FRIO";
+        } else if (diferencia >= 16 && diferencia <= 30)
+        {
+            return "TIBIO";
+        } else if (diferencia >= 6 && diferencia <= 15)
+        {
+            return "CALIENTE";
+        } else if (diferencia <= 5 && diferencia >= 1)
+        {
+            return "MUY CALIENTE";
+        } else
+        {
+            return "EL NUMERO INGRESADO ES EL CORRECTO";
+        }
+    }
+
+    public Boolean FinalizoElJuego(int _numeroRandom, int _numeroIngresado)
+    {
+        if(_numeroRandom == _numeroIngresado)
+        {
+            return true;
+        }    
+        return false;
+    }
+}
