dec87175f7fe853556b4a10b8daeb008aff1eaec
nseehofer <nicolasseehofer.99@hotmail.com>
2025-08-26 17:26:59 -0300

    Juego de Simon dice casi terminado, queda reescribir el metodo para verificar secuencia

---
+++ PracticaClase1/PracticaClase1.Consola/SimonDiceJuego.cs
@@ -0,0 +1,28 @@
+﻿using PracticaClase1.Logica;
+
+        SimonDice simonDice = new SimonDice();
+        simonDice.IniciarJuego();
+
+        Console.WriteLine("Simon dice... " + simonDice.ObtenerSecuencia());
+        Console.Write("¿¿¿ Que dijo Simon ???");
+
+        // Obtener la secuencia ingresada por el usuario y guardarla en un ArrayList de string
+        string secuenciaUsuario = Console.ReadLine();
+        List<string> secuenciaUsuarioList = new List<string>();
+        if (secuenciaUsuario != null)
+        {
+            secuenciaUsuarioList = secuenciaUsuario.Split(' ').ToList();
+        }
+
+        if (simonDice.VerificarSecuencia(secuenciaUsuarioList))
+        {
+            Console.WriteLine("¡Correcto! Has seguido la secuencia.");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("¡Incorrecto! La secuencia era: " + simonDice.ObtenerSecuencia());
+            Console.WriteLine("No te desanimes, podes volver a jugar!");
+            simonDice.ReiniciarJuego();
+        }
+ 
---
+++ PracticaClase1/PracticaClase1.Logica/SimonDice.cs
@@ -0,0 +1,102 @@
+﻿using System.Drawing;
+namespace PracticaClase1.Logica;
+
+public class SimonDice
+{
+    // Disponibilizar las 4 flechas del teclado en un arraylist, todo dentro de un atributo privado y estatico 
+    private static string[] _letras = { "a", "d", "p", "w" };
+
+    private static int _cantidadDeJuegos;
+
+    // Atributo privado en un arraylist que guarde la secuencia de flechas
+    private List<string> _secuencia;
+
+    // Con esta sintaxis solo tengo la posibilidad de lectura para obtener la secuencia
+    public List<string> Secuencia => _secuencia;
+
+    public SimonDice()
+    {
+        _cantidadDeJuegos = 0;
+        _secuencia = new List<string>();
+    }
+
+    public void IniciarJuego()
+    {
+        _cantidadDeJuegos++;
+        VaciarSecuencia();
+        AgregarFlechaAleatoriaALaSecuencia(_cantidadDeJuegos);
+    }
+
+    private void AgregarFlechaAleatoriaALaSecuencia(int vecesJugadas)
+    {
+
+        if (vecesJugadas >= 1 && vecesJugadas <= 4)
+        {
+            for (int i = 0; i < (vecesJugadas + 1); i++)
+            {
+                _secuencia.Add(_letras[GenerarRandom()] + " ");
+            }
+
+        }
+        else if (vecesJugadas >= 5 && vecesJugadas <= 9)
+        {
+            for (int i = 0; i < (vecesJugadas + 1); i++)
+            {
+                _secuencia.Add(_letras[GenerarRandom()] + " ");
+            }
+        }
+        else if (vecesJugadas >= 10)
+        {
+            for (int i = 0; i < (vecesJugadas + 1); i++)
+            {
+                _secuencia.Add(_letras[GenerarRandom()] + " ");
+            }
+        }
+
+
+    }
+
+    private int GenerarRandom()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_letras.Length);
+        return index;
+    }
+
+    
+    private void VaciarSecuencia()
+    {
+        _secuencia.Clear();
+    }
+
+    public string ObtenerSecuencia()
+    {
+        return string.Join(" ", _secuencia);
+    }
+
+    // REESCRIBIR EL CRITERIO DE VERIFICACION DE LA SECUENCIA
+    public bool VerificarSecuencia(List<string> secuenciaUsuario)
+    {
+        if (secuenciaUsuario.Count != _secuencia.Count)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < _secuencia.Count; i++)
+        {
+            if (secuenciaUsuario[i] != _secuencia[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void ReiniciarJuego()
+    {
+        _cantidadDeJuegos = 0;
+        VaciarSecuencia();
+    }
+
+}
