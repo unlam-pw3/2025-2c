0ca0d439f6083ac52c509aa28df0831baa57db6b
Carla <101598008+Carla-Stramandinoli@users.noreply.github.com>
2025-08-26 18:14:38 -0300

    resolucion de ejecicio - Carla Stramandinoli

---
+++ Clase1/EjercicioNumOculto.Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using EjercicioNumOculto.Logica;
+
+JuegoAdivinarNumero juegoAdivinarNumero = new JuegoAdivinarNumero();
+int _numRandom = juegoAdivinarNumero.GetNumeroRandom();
+int _numUsuario = 0;
+
+
+while (!juegoAdivinarNumero.JuegoTerminado(_numRandom, _numUsuario))
+{
+    Console.Write("Ingrese un numero del 1 al 100: ");
+    _numUsuario = int.Parse(Console.ReadLine());
+    Console.WriteLine(juegoAdivinarNumero.IndicaCercaniaAlUsuario(_numUsuario, _numRandom));
+}
+
+Console.WriteLine("Numero correcto, juego terminado");
---
+++ Clase1/EjericioNumOculto.Logica/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace EjercicioNumOculto.Logica;
+public class JuegoAdivinarNumero
+{
+    private int _numeroMinimo = 1;
+    private int _numeroMaximo = 100;
+
+    public int GetNumeroRandom()
+    {
+        Random numRandom = new Random();
+        return numRandom.Next(_numeroMinimo, _numeroMaximo);
+    }
+
+    public string IndicaCercaniaAlUsuario(int _numUsuario, int _numRandom)
+    {
+        int diferencia = Math.Abs(_numUsuario - _numRandom);
+
+        if (diferencia > 30) return "Frío";
+
+        if (diferencia >= 16 && diferencia <= 30) return "Tibio";
+
+        if (diferencia >= 6 && diferencia <= 15) return "Caliente";
+
+        if (diferencia <= 5 && diferencia >= 1) return "Muy caliente";
+
+        return "Numero invalido";
+
+    }
+
+    public bool JuegoTerminado(int _numRandom, int _numUsuario)
+    {
+        if (_numUsuario == _numRandom)
+        {
+            return true;
+        }
+        return false;
+    }
+}
