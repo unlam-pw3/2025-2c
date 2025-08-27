5f9ea07131734bc40740409d5005647335f170b6
mateo <mateo.fortu@gmail.com>
2025-08-27 09:25:36 -0300

    feat: juego de adivinar número sin comentarios

---
+++ AdivinarNumero.Logica/GuessResult.cs
@@ -0,0 +1,10 @@
+﻿namespace AdivinarNumero.Logica;
+
+public enum GuessResult
+{
+    Correcto,
+    MuyCaliente, // diff <= 5 (y != 0)
+    Caliente,    // 6..15
+    Tibio,       // 16..30
+    Frio         // > 30
+}
