2651bf5deb933d9be7b098803bf011498d9b0706
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2025-09-17 20:12:15 -0300

    Clase 4

---
+++ Clase4/CLASE4_POO_Entidades/Pato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASE4_POO_Entidades
+{
+    public class Pato : Animal
+    {
+        public Pato(string nombre) : base(nombre)
+        {
+        }
+
+        public override string HacerSonido()
+        {
+            return "Cuac Cuac";
+        }
+
+        public override int ObtenerCantidadDePatas()
+        {
+            return base.setCantidadDePatas("bipedo");
+        }
+    }
+}
---
+++ Clase4/CLASE4_POO_Servicio/AnimalesServicio.cs
@@ -0,0 +1,33 @@
+﻿using CLASE4_POO_Entidades;
+namespace CLASE4_POO_Servicio
+{
+    public interface IAnimalesServicio
+    {
+        List<Animal> ObtenerAnimales();
+        void AgregarAnimal(Animal animal);
+    }
+
+    public class AnimalesServicio : IAnimalesServicio
+    {
+        private List<Animal> _animales;
+
+        public AnimalesServicio()
+        {
+            _animales = new List<Animal>();
+            // Agregar algunos animales de ejemplo
+            _animales.Add(new Vaca("Lola"));
+            _animales.Add(new Pato("Donald"));
+            _animales.Add(new Vaca("Margarita"));
+            _animales.Add(new Pato("Pato Lucas"));
+        }
+        public List<Animal> ObtenerAnimales()
+        {
+            return _animales;
+        }
+        public void AgregarAnimal(Animal animal)
+        {
+            _animales.Add(animal);
+        }
+
+    }
+}
