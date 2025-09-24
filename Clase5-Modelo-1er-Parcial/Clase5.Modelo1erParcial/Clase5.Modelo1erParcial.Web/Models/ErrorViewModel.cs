f421272114b9f8f632e227ab8edd6324edd094a3
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2025-09-24 19:30:10 -0300

    eliminando archivos innecesarios

--- Clase5-Modelo-1er-Parcial/Clase5.Modelo1erParcial/Clase5.Modelo1erParcial.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Clase5.Modelo1erParcial.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
