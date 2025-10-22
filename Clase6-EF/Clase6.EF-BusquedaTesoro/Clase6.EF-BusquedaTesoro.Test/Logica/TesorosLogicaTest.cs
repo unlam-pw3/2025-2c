8b69558b0ae650e8a0f21f43db70e6652cd33197
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2025-10-22 20:44:58 -0300

    Agregando Tests

---
+++ Clase6-EF/Clase6.EF-BusquedaTesoro/Clase6.EF-BusquedaTesoro.Test/Logica/TesorosLogicaTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase6.EF_BusquedaTesoro.Data.Entidades;
+using Clase6.EF_BusquedaTesoro.Logica;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clase6.EF_BusquedaTesoro.Test.Logica;
+
+public class TesorosLogicaTest : TestBase
+{
+    [Fact]
+    public void AgregarTest()
+    {
+        Pw320252cBusquedaTesoroContext context = ServiceProvider.GetService<Pw320252cBusquedaTesoroContext>();
+        ITesorosLogica tesorosLogica = new TesorosLogica(context);
+
+        //Arrange
+        var tesoro = new Data.Entidades.Tesoro
+        {
+            Nombre = "Tesoro1",
+            Valor = 1000,
+            ImagenUrl = "http://imagen.com/tesoro1.jpg",
+            Latitud = 10.0M,
+            Longitud = 20.0M
+        };
+        //Act
+        tesorosLogica.AgregarTesoro(tesoro);
+        var tesoros = tesorosLogica.ObtenerTesoros();
+
+        //Assert
+        Assert.Single(tesoros);
+        Assert.Equal("Tesoro1", tesoros[0].Nombre);
+    }
+
+    //Eliminar
+    [Fact] 
+    public void EliminarTest()
+    {
+        Pw320252cBusquedaTesoroContext context = ServiceProvider.GetService<Pw320252cBusquedaTesoroContext>();
+        ITesorosLogica tesorosLogica = new TesorosLogica(context);
+        //Arrange
+        var tesoro = new Data.Entidades.Tesoro
+        {
+            Nombre = "Tesoro1",
+            Valor = 1000,
+            ImagenUrl = "http://imagen.com/tesoro1.jpg",
+            Latitud = 10.0M,
+            Longitud = 20.0M
+        };
+        tesorosLogica.AgregarTesoro(tesoro);
+        //Act
+        tesorosLogica.EliminarTesoro(tesoro.IdTesoro);
+        var tesoros = tesorosLogica.ObtenerTesoros();
+        //Assert
+        Assert.Empty(tesoros);
+    }
+
+    //actualizar
+    [Fact]
+    public void ActualizarTest()
+    {
+        Pw320252cBusquedaTesoroContext context = ServiceProvider.GetService<Pw320252cBusquedaTesoroContext>();
+        ITesorosLogica tesorosLogica = new TesorosLogica(context);
+        //Arrange
+        var tesoro = new Data.Entidades.Tesoro
+        {
+            Nombre = "Tesoro1",
+            Valor = 1000,
+            ImagenUrl = "http://imagen.com/tesoro1.jpg",
+            Latitud = 10.0M,
+            Longitud = 20.0M
+        };
+        tesorosLogica.AgregarTesoro(tesoro);
+        //Act
+        tesoro.Nombre = "TesoroActualizado";
+        tesorosLogica.ActualizarTesoro(tesoro);
+        var tesoroActualizado = tesorosLogica.ObtenerTesoroPorId(tesoro.IdTesoro);
+        //Assert
+        Assert.Equal("TesoroActualizado", tesoroActualizado.Nombre);
+    }
+}
---
+++ Clase6-EF/Clase6.EF-BusquedaTesoro/Clase6.EF-BusquedaTesoro.Test/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using Clase6.EF_BusquedaTesoro.Data.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clase6.EF_BusquedaTesoro.Test;
+
+public class TestBase
+{
+    protected ServiceCollection Services { get; }
+    protected ServiceProvider ServiceProvider { get; private set; }
+
+    public TestBase()
+    {
+        Services = new ServiceCollection();
+
+        Services.AddEntityFrameworkInMemoryDatabase();
+
+        Services.AddDbContext<Pw320252cBusquedaTesoroContext>((sp, options) =>
+            options.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .UseInternalServiceProvider(sp));
+
+        ServiceProvider = Services.BuildServiceProvider();
+    }
+}
