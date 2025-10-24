1e615a3ba732f247431f8f1e4407b0e0eeb6783c
fgp_2001 <fgp_2001@hotmail.com>
2025-10-24 13:36:56 -0300

    Se implemento el Alta, Baja, Modificación del Cazatesoros

---
+++ Clase6-EF/Clase6.EF-BusquedaTesoro/Clase6.EF-BusquedaTesoro.Data/Entidades/Cazatesoro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clase6.EF_BusquedaTesoro.Data.Entidades;
+
+public partial class Cazatesoro
+{
+    public int IdCazatesoro { get; set; }
+
+    public string Nick { get; set; } = null!;
+
+    public string? AvatarUrl { get; set; }
+}
--- Clase6-EF/Clase6.EF-BusquedaTesoro/Clase6.EF-BusquedaTesoro.Data/Entidades/Pw320252cBusquedaTesoroContext.cs
+++ Clase6-EF/Clase6.EF-BusquedaTesoro/Clase6.EF-BusquedaTesoro.Data/Entidades/Pw320252cBusquedaTesoroContext.cs
@@ -16,10 +16,12 @@ public partial class Pw320252cBusquedaTesoroContext : DbContext
     }
 
     public virtual DbSet<Tesoro> Tesoros { get; set; }
+    public virtual DbSet<Cazatesoro> Cazatesoros { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=PW3-2025-2c-BusquedaTesoro;Trusted_Connection=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-TIL47OO;Database=PW3-2025-2c-BusquedaTesoro;Trusted_Connection=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -33,6 +35,20 @@ public partial class Pw320252cBusquedaTesoroContext : DbContext
             entity.Property(e => e.Latitud).HasColumnType("decimal(9, 6)");
             entity.Property(e => e.Longitud).HasColumnType("decimal(9, 6)");
             entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Cazatesoro>(entity =>
+        {
+            entity.HasKey(e => e.IdCazatesoro);
+
+            entity.ToTable("Cazatesoro");
+
+            entity.Property(e => e.Nick)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            entity.Property(e => e.AvatarUrl)
+                .HasMaxLength(1000);
         });
 
         OnModelCreatingPartial(modelBuilder);
